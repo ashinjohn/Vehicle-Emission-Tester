048c6c2937cef1b0dfa4a2a62266c1619f001a7a
ashinjohn <ashinbasiljohn@gmail.com>
2017-04-01 21:45:43 +0530

    Sensors with GSM

--- Emission Check.c
+++ Emission Check.c
@@ -5,62 +5,33 @@
 volatile unsigned char value;
 volatile unsigned int O_flag=0,K_flag=0,send_flag=0;
 
-int horse=0;
-int cat=0;
 
-int read_adc (char chn1)
-{
-
-int val , val1;
-
-
-ADMUX = 0x40 ; 
-ADCSRA = 0x86 ; 
-			  
-			   
-
-ADMUX = (0x40|(chn1&0x07)); 
-							
-
-	
-
-_delay_ms(1); 
-
-ADCSRA |= 0x40; 
-
-while ((ADCSRA&0x40)); 
-					   
-val=ADCL;
-val1=ADCH;
-
-val= val|(val1<<8); 
-return (val);
-
-
-}
-
+void LCD_cmd(char cmd);
+void LCD_dat(char dat);
+void printlcd(const char *CPtr);
 
 
 
 ISR(USART_RXC_vect)
 {
 
-value=UDR;
+	value=UDR;
 
-if(value=='O')
-O_flag=1;
+	if(value=='O')
+	O_flag=1;
 
-else if((value=='K')&&(O_flag==1))
-{
-K_flag=1;
-cat=1;
-send_flag=1;
-}
+	else if((value=='K')&&(O_flag==1))
+	{
+		K_flag=1;
+		O_flag=0;
+	}
+	else if(value=='>')
+	send_flag=1;
 
 
-UCSRA=0x40;
-UDR=value;
-while((UCSRA&0x40)==0);
+	UCSRA=0x40;
+	UDR=value;
+	while((UCSRA&0x40)==0);
 }
 
 
@@ -90,97 +61,196 @@ UCSRC=0x86;
 
 
 
-prints("ATE0\r\n");
-prints("AT+CMGF=1\r\n");
-prints("AT+CNMI=2,1,0,0,0\r\n");
-prints("AT+CMGS=\"+919400927068\"\r\n");
+
+	DDRC=0xFF;
+	DDRD=0xE0;
+	DDRA=0x00;
+
+
+    LCD_cmd(0x30);
+	LCD_cmd(0x30);
+	LCD_cmd(0x30);
+
+	LCD_cmd(0x38);
+
+	LCD_cmd(0x38);
+	LCD_cmd(0x06);
+	LCD_cmd(0x0C);
+ 
+    LCD_cmd(0x01);
+	LCD_cmd(0x80);
+
+
+	printlcd("SYSTEM READY");
+	LCD_cmd(0xC0);
+	printlcd("Monitoring Level");
+	_delay_ms(1000);
+
+	_delay_ms(1000);
 
 sei();
 
 
-char buf[6];
-int cnt=0;
-int adc_val=0;
+int pro1 =0;
+int pro2 =0;
 
 
-int sens1,sens2;
-int problem;
 
-int pre1,pre2;
 
 while(1)
 {
 
-	adc_val=read_adc(0);   // reading data from sensor 1 
-		sens1=adc_val;
-		
-		if (horse==0)
-		{ pre1=sens1-50; }
-		
-		
-		
-		buf[4]=0;
-		for (cnt=3;cnt>=0;cnt--)
-		{
-		 buf[cnt]=(adc_val%10)+'0';
-		 adc_val=adc_val/10;
-		}
-
-			for (cnt=1;cnt<=3;cnt++)
-		{
-		 	send_char(buf[cnt]);
-		}
-
-		send_char(' ');
-	
-	
-	adc_val=read_adc(1);   // reading data from sensor 2
-		sens2=adc_val;
-		
-			if (horse==0)
-			{ pre2=sens2-30; }
-		
-		
-		buf[4]=0;
-		for (cnt=3;cnt>=0;cnt--)
-		{
-		 buf[cnt]=(adc_val%10)+'0';
-		 adc_val=adc_val/10;
-		}
-	for (cnt=1;cnt<=3;cnt++)
-		{
-		 	send_char(buf[cnt]);
-		}
-
-    
-	
-	
-	
-	horse=1;
-
-
-
-	if (sens1<pre1)
-	problem=1;
-	else 
-	problem=0;
-
-
-
-send_char('\r');
-send_char('\n');
-
-
-if (problem==1)
+if((PINA & 0x01)==0)
 {
-prints(" White Maruthi Suzuki Swift KL 40 A 1717 ");
-send_char(0x1A);
-send_flag=0;
+pro1=1;
+    LCD_cmd(0x01);
+	LCD_cmd(0x80);
+
+
+	printlcd("HIGH");
+	LCD_cmd(0xC0);
+	printlcd("Carbon Dioxide");
+		_delay_ms(1000);
+}
+
+else
+{
+    pro1=0;
+	LCD_cmd(0x01);
+	LCD_cmd(0x80);
+
+
+	printlcd("LOW");
+	LCD_cmd(0xC0);
+	printlcd("Carbon Monoxide");
+		_delay_ms(1000);
+
+}
+
+_delay_ms(1000);
+
+if((PINA & 0x02)==0)
+{
+pro2=1;
+
+    LCD_cmd(0x01);
+	LCD_cmd(0x80);
+
+
+	printlcd("HIGH");
+	LCD_cmd(0xC0);
+	printlcd("Carbon Monoxide");
+		_delay_ms(1000);
+}
+else
+{
+    pro2=0;
+	LCD_cmd(0x01);
+	LCD_cmd(0x80);
+
+
+	printlcd("LOW");
+	LCD_cmd(0xC0);
+	printlcd("Carbon Dioxide");
+		_delay_ms(1000);
+
+}
+_delay_ms(1000);
+
+if (pro1==1||pro2==1)
+{
+
+    LCD_cmd(0x01);
+	LCD_cmd(0x80);
+
+
+	printlcd("Alerting");
+	LCD_cmd(0xC0);
+	printlcd("Authority");
+	_delay_ms(1000);
+
+   LCD_cmd(0x01);
+	LCD_cmd(0x80);
+
+
+
+		printlcd("Sending");
+	LCD_cmd(0xC0);
+	printlcd("Text Message");
+	_delay_ms(1000);
+
+ sei();    // global interrupt enable
+
+
+
+			while(K_flag==0)
+			{
+				prints("AT\r\n");
+				_delay_ms(2000);
+			}
+
+			prints("ATE0\r\n");
+			_delay_ms(2000);
+			prints("AT+CMGF=1\r\n");
+			_delay_ms(2000);
+			prints("AT+CNMI=2,1,0,0,0\r\n");
+			_delay_ms(2000);
+			prints("AT+CMGS=\"+919544907374\"\r\n");
+			_delay_ms(2000);
+			while(1)
+			{
+				if(send_flag==1)
+				{
+					send_flag==0;
+					prints("Maruthi Suzuki Swift KL7 1717 White");
+					
+					_delay_ms(1000);
+					send_char(0x1A);
+					
+				}
+			}
+
+   LCD_cmd(0x01);
+	LCD_cmd(0x80);
+
+
+	printlcd("MESSAGE SENT");
+
+	_delay_ms(1000);
+
 while(1);
-}
-}
-}
+} //probelm if 
+
+}//while1  
+} //main
 
 
 
+void LCD_cmd(char cmd)
+{
+	PORTD&=(~0x80);
+	PORTD&=(~0x40);
+	PORTC=cmd;
+	PORTD|=0x20;
+	PORTD&=(~0x20);
+	_delay_ms(10);
+}
+
+void LCD_dat(char dat)
+{
+	PORTD|=0x80;
+	PORTD&=(~0x40);
+	PORTC=dat;
+	PORTD|=0x20;
+	PORTD&=(~0x20);
+	_delay_ms(10);
+}
+
+void printlcd(const char *CPtr)
+{
+	while(*CPtr != '\0'){
+		LCD_dat(*CPtr);
+		++CPtr;
+	}
+}
 
